3e19e0dc876dcdd28cdcdaf831c3c3467791b35b
SirRuBick <shanrb119@gmail.com>
2024-01-12 00:55:23 +1100

    autocommands to hightlight active window

--- lua/nvshan/autocmds.lua
+++ lua/nvshan/autocmds.lua
@@ -7,21 +7,51 @@
 --
 
 local is_available = require("utils").is_available
-local enable_transparent = require("settings").transparent
+local is_transparent = require("settings").transparent
 
-
--- auto close NvimTree
+-- Auto close NvimTree
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
-  pattern = "NvimTree_*",
-  callback = function()
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if
-        layout[1] == "leaf"
-        and vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_win_get_buf(layout[2]) }) == "NvimTree"
-        and layout[3] == nil
-    then
-      vim.api.nvim_command([[confirm quit]])
-    end
-  end,
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_win_get_buf(layout[2]) }) == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.api.nvim_command([[confirm quit]])
+		end
+	end,
+})
+
+-- Auto resize split
+vim.api.nvim_create_autocmd("VimResized", {
+	group = vim.api.nvim_create_augroup("ResizeSplit", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_command([[wincmd =]])
+	end,
+})
+
+-- Distinguish active and inactive window
+vim.api.nvim_create_autocmd("WinEnter", {
+	group = vim.api.nvim_create_augroup("SetCulInActiveWindow", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_command([[setlocal cursorline]])
+		vim.api.nvim_command([[setlocal number]])
+		vim.api.nvim_command([[setlocal relativenumber]])
+		-- 			vim.api.nvim_command([[highlight! Normal guibg=#1e222a]])
+	end,
+})
+vim.api.nvim_create_autocmd("WinLeave", {
+	group = vim.api.nvim_create_augroup("UnsetCulInActiveWindow", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_command([[setlocal nocursorline]])
+		vim.api.nvim_command([[setlocal nonumber]])
+		vim.api.nvim_command([[setlocal norelativenumber]])
+		-- 			vim.api.nvim_command([[highlight! Normal guibg=#1e222a]])
+	end,
 })
