0966d2777e8eac5b5caaea5025c969ef3d34a082
SirRuBick <shanrb119@gmail.com>
2024-01-12 00:54:32 +1100

    more copilot keymaps

--- lua/nvshan/plugins/completion/keymap.lua
+++ lua/nvshan/plugins/completion/keymap.lua
@@ -5,7 +5,10 @@ local map = keymap_utils.map
 
 if is_available("copilot.vim") then
 	mappings.i["<M-;>"] = map('copilot#Accept("<CR>")'):expr():desc("Copilot Accept")
+	mappings.i["<M-'>"] = map(("<Plug>(copilot-dismiss)")):desc("Copilot Dismiss")
 	mappings.n["<leader>mv"] = map("<CMD>Copilot panel<CR>"):desc("Coplit Panel")
+  mappings.i["<M-Down>"] = map("<Plug>(copilot-next)"):desc("Copilot Next")
+  mappings.i["<M-Up>"] = map("<Plug>(copilot-previous)"):desc("Copilot Previous")
 end
 
 return mappings
